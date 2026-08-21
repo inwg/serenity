ccf0bb05c6f19dbae196f7640a1140996a109b1b
inwg <109669438+inwg@users.noreply.github.com>
2026-08-21 19:39:06 +0800

    forgot to fix ts

--- loader.lua
+++ loader.lua
@@ -1,38 +1,31 @@
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
 local function getGameId()
     return game.PlaceId or game.GameId or 0
 end
-
 local function kickPlayer(message)
-    local player = game.Players.LocalPlayer
-    if player and player:Kick then
+    local player = Players.LocalPlayer
+    if player and player.Kick then
         player:Kick(message)
-    ==[[
     else
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Serenity",
-            Text = message,
-            Duration = 5
-        })
-        ]]
+        pcall(function()
+            StarterGui:SetCore("SendNotification", {
+                Title = "Serenity",
+                Text = message,
+                Duration = 5
+            })
+        end)
         wait(2)
         game:Shutdown()
     end
 end
-
 local GAME_ID = getGameId()
-
--- 1.8 Arena
 if GAME_ID == 77790193039862 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/inwg/serenity/refs/heads/main/game/1.8-arena/beta.lua"))()
-
--- Prison Life
 elseif GAME_ID == 135564683255158 or GAME_ID == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/inwg/serenity/refs/heads/main/game/prisonlife/prisonlife.lua"))()
-
--- BloxStrike
 elseif GAME_ID == 114234929420007 then
     kickPlayer("BloxStrike is under maintenance.")
-
 else
     kickPlayer("Game is not supported.")
 end
